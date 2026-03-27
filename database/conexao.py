f90d410474206b574e126d045ecbf6d39065c8ba
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-03-27 15:42:52 -0300

    ADD: Diferentes locais

--- database/conexao.py
+++ database/conexao.py
@@ -5,13 +5,24 @@ class Conexao():
     # transforma em metodo estático, só precisa chama-lo
     @staticmethod
     def conectar():
-        con = mysql.connector.connect(
-            host="localhost",
-            port = 3306,
-            user = "root",
-            password="root",
-            database = "NaclMusic"
-        )
-        cur = con.cursor(dictionary=True)
+        tipo_local = "nuvem"
+        if tipo_local == "local":
+            con = mysql.connector.connect(
+                host="localhost",
+                port = 3306,
+                user = "root",
+                password="root",
+                database = "NaclMusic"
+            )
+            cur = con.cursor(dictionary=True)
+        else:
+            con = mysql.connector.connect(
+                host="localhoserver-nacl-nacl-music.a.aivencloud.comst",
+                port = 23340,
+                user = "avnadmin",
+                password="AVNS_2VJEs0HZhnTymSnnDJj",
+                database = "defaultdb"
+            )
+            cur = con.cursor(dictionary=True)
 
         return con, cur
